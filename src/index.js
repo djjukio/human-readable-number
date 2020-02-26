255b52a9faa7b3e09e395e18ffdb8d8a6d23c8d2
Andrey Alfirovich <djjukio@gmail.com>
2020-02-26 22:07:17 +0300

    feat: add solition task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
+const NUMBER = [
+    'zero',
+    'one',
+    'two',
+    'three', 
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen', 
+    'sixteen', 
+    'seventeen',
+    'eighteen', 
+    'nineteen'],
+  NUMBER_TENS = [
+    'twenty',
+    'thirty', 
+    'forty', 
+    'fifty', 
+    'sixty', 
+    'seventy',
+    'eighty', 
+    'ninety'
+    ],
+  NUMBER_HUNDRED = 'hundred';
+
 module.exports = function toReadable (number) {
-  
+if (number >= 1000) {
+throw 'Too big number';
+}
+
+let result = [];
+
+if (number >= 100) {
+result.push(NUMBER[Math.floor(number / 100)]);
+result.push(NUMBER_HUNDRED);
+
+number = number % 100;
+}
+
+if (number >= 20) {
+result.push(NUMBER_TENS[Math.floor(number / 10) - 2]);
+if (number % 10 > 0) {
+  result.push(NUMBER[number % 10]);
+}
+} else if (number > 0 || result.length === 0) {
+result.push(NUMBER[number]);
+}
+
+return result.join(' ');
 }
